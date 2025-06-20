d1752dccd272a60536ffe8795fe2eab45c112e5d
ManojkumarSri <kumarsrinivasanmanoj@gmail.com>
2025-06-20 13:30:14 +0530

    Manoj added few lines

---
+++ src/org/pro/Testing.java
@@ -0,0 +1,23 @@
+package org.pro;
+
+public class Testing {
+	
+	public static void main(String[] args) {
+		
+		
+		String s = "Manojkumar@5";
+		
+		String s2 = s.replaceAll("[0-5]", "");
+		
+		System.out.println(s2);
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+
+}
