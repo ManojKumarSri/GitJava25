4c70a6363af459c33b9d8250f7a7ebeaac243148
ManojkumarSri <kumarsrinivasanmanoj@gmail.com>
2025-06-23 20:17:05 +0530

    Added additional methods

--- src/org/pro/Testing.java
+++ src/org/pro/Testing.java
@@ -7,9 +7,15 @@ public class Testing {
 		
 		String s = "Manojkumar@5";
 		
+		StringBuilder sb = new StringBuilder(s);
+		
+		StringBuilder rev = sb.reverse();
+		
+	    System.out.println("Reverse order of String:"+rev);
+		
 		String s2 = s.replaceAll("[0-5]", "");
 		
-		System.out.println(s2);
+		System.out.println("Numeric removed:"+s2);
 		
 		
 		
